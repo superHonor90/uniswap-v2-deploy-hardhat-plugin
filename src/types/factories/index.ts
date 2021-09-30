717a793c74120d845e46ea86f4829c594b024de7
Alex Harley <alexbruceharley@gmail.com>
2021-09-30 14:32:38 +0200

    chore: adding typechain

---
+++ src/types/factories/index.ts
@@ -0,0 +1,4 @@
+export * from "./IUniswapV2Factory__factory";
+export * from "./IUniswapV2Pair__factory";
+export * from "./IUniswapV2Router01__factory";
+export * from "./IUniswapV2Router02__factory";
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,1 +1,6 @@
 export * from "./IWETH9";
+export * from "./IUniswapV2Factory";
+export * from "./IUniswapV2Pair";
+export * from "./IUniswapV2Router01";
+export * from "./IUniswapV2Router02";
+export * from "./factories";
